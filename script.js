65e3fcd3c4fdc6f0e5a3b21866ddc172790992bd
UlaMaarij <ulamaarijirsyad@gmail.com>
2026-03-18 19:44:24 +0700

    penambahan hero tier

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// SEARCH HERO
 function searchHero() {
 
 let input = document.getElementById("searchHero").value.toLowerCase();
@@ -17,3 +18,46 @@ hero.style.display = "none";
 });
 
 }
+
+
+// DRAG & DROP HERO TIER
+
+// ambil hero
+const heroes = document.querySelectorAll(".hero-card");
+
+// ambil semua area drop (tier + hero pool)
+const dropZones = document.querySelectorAll(".tier, .hero-pool");
+
+heroes.forEach(hero => {
+
+hero.setAttribute("draggable", true);
+
+hero.addEventListener("dragstart", function(){
+this.classList.add("dragging");
+});
+
+hero.addEventListener("dragend", function(){
+this.classList.remove("dragging");
+});
+
+});
+
+
+// semua area bisa menerima drop
+dropZones.forEach(zone => {
+
+zone.addEventListener("dragover", function(e){
+e.preventDefault();
+});
+
+zone.addEventListener("drop", function(){
+
+const hero = document.querySelector(".dragging");
+
+if(hero){
+this.appendChild(hero);
+}
+
+});
+
+});
